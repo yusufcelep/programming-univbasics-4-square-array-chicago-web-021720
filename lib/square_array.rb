fa250ab77297f39c4dbde2ce7741dc63113d508f
Yusuf Celep <yusufcelep8@gmail.com>
2020-01-29 08:42:50 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   counter = 0
  
   while counter < array.length do
-    new array[counter] = array[counter] * array[counter]
+    puts array[counter] = array[counter] * array[counter]
     counter += 1
   end
 end
